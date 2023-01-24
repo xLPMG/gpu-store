dd5ef1873782667eca64788635bf6393b3cb175e
Luca G <xxlpmghdxx@gmail.com>
2023-01-24 15:37:43 +0100

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Footer.module.css';
+const Footer = () => {
+ return (
+    <footer className={styles.footer}>
+       
+    </footer>
+ );
+};
+export default Footer;
---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>ABOUT</h1>
+      </main>
+    </>
+  )
+}
